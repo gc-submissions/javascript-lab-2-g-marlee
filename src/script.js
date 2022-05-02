240ac8d6c804b7e3cc318fde585c81b5167321ba
Marlee Garcia <marlee.garcia121@gmail.com>
2022-05-02 19:25:49 -0400

    Created chooseOption function

--- src/script.js
+++ src/script.js
@@ -3,3 +3,12 @@
 let randomDamage = () => {
     return(Math.floor(Math.random() * 10) +1);
 }
+
+let chooseOption = (opt1, opt2) => {
+    let randNum = Math.floor(Math.random() * 2)
+    if (randNum === 0) {
+        return opt1;
+    } else {
+        return opt2;
+    }
+}
