8102399e9d09f80d1fdff81b6518ed92f59c78c2
Marlee Garcia <marlee.garcia121@gmail.com>
2022-05-02 20:11:49 -0400

    Created fight function

--- src/script.js
+++ src/script.js
@@ -32,3 +32,24 @@ let isDead = (health) => {
         return false;
     }
 }
+
+function fight(player1, player2, player1Health, player2Health) {
+    while(true) {
+        let attacker = chooseOption(player1, player2);
+        if (attacker === player1) {
+            player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if (isDead(player2Health) === true) {
+                logDeath(player1, player2);
+                break;
+            }
+        } else {
+            player1Health = attackPlayer(player1Health);
+            logHealth(player1, player1Health);
+            if (isDead(player2Health) === true) {
+                logDeath(player2, player1)
+                break;
+            }
+        }
+    }
+}
